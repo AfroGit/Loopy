fc0494c3bdcae5d79b41ef088242eb2b7349a313
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-02 21:05:50 +0000

    JavaScript has a Math.random() function that generates a random decimal number between 0 (inclusive) and 1 (exclusive). Thus Math.random() can return a 0 but never return a 1.check out line 71

--- index.js
+++ index.js
@@ -13,16 +13,69 @@
 
 /*Push the odd numbers from 1 through 9 to myArray using a for loop. */ 
 
-const myArray = [];
-for(let i = 1; i <= 9; i += 2){
-  myArray.push(i);
-  };
-console.log(myArray);
+// const myArray = [];
+// for(let i = 1; i <= 9; i += 2){
+//   myArray.push(i);
+//   };
+// console.log(myArray);
 // ------------------------------------------
 /*Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total. */ 
-const myArr = [2, 3, 4, 5, 6];
-let total = 0;
-for(let i = 0;i < myArr.length;i++){
-  total += myArr[i];//total + myArr[0] -> 0+2 = 2  
+// const myArr = [2, 3, 4, 5, 6];
+// let total = 0;
+// for(let i = 0;i < myArr.length;i++){
+//   total += myArr[i];//total + myArr[0] -> 0+2 = 2  
+// };
+// console.log(total);
+
+// -----------------------------------------------
+// const arr = [
+//   [1, 2], [3, 4], [5, 6]
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]);
+//   }
+// }
+// ------------------------------------------
+// function multiplyAll(arr) {
+//   var product = 1;
+//   // Only change code below this line
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < arr[i].length; j++) {
+//       product *= arr[i][j];
+//     }
+//   }
+//   // Only change code above this line
+//   console.log(product);
+// }
+
+// Modify values below to test your code
+// multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// --------------------------------------------
+
+
+// function countdown(i) {console.log(i)//print out the number 5
+//     if (i <= 1) {  // base case
+//         return;
+//     } else {     // recursive case
+//         countdown(i - 1);
+//     }
+//   // console.log(i)
+// }
+
+// countdown(5);
+
+/*Change randomFraction to return a random number instead of returning 0. */ 
+function randomFraction() {
+
+  
+      let result = 0;
+      while(result === 0){
+        result = Math.random();
+
+      }
+  return result;
 };
-console.log(total);
+console.log(randomFraction(1));
